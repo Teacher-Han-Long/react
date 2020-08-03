2ff0f81db7877466b3a43e33d9ecadf7bd49e088
Russell Davis <31553582+Teacher-Han-Long@users.noreply.github.com>
2020-08-03 10:29:22 -0500

    Add files via upload

---
+++ sm_react_proj/lotto_ball/lotto-ball/src/From.js
@@ -0,0 +1,24 @@
+// function arrayFromArgs() {
+//     return Array.from(arguments);
+// }
+
+// console.log(arrayFromArgs(1, 'A'));
+
+// function timesHundred() {
+//      return Array.from(arguments, n => n * 100);
+// }
+
+// const newArr = timesHundred(5, 20, 100);
+// console.log(newArr);
+
+// let even = {
+//     *[Symbol.iterator]() {
+//         for (let i = 0; i < 10; i += 2) {
+//             yield i;
+//         }
+//     }
+// }
+// let evenNumbers = Array.from(even);
+// console.log(evenNumbers);
+
+console.log(Array);
